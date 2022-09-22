7d20a785914b041b755d64eedaa58567345c017b
scw3812 <scw3812@naver.com>
2022-09-22 19:20:48 +0900

    sign up api

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -10,5 +11,10 @@ export class CatsController {
   @Get()
   getCurrentCat() {
     return 'current cat';
+  }
+
+  @Post()
+  async signUp(@Body() body: CatRequestDto) {
+    return await this.castsService.signUp(body);
   }
 }
