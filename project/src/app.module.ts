1c6b2eee2d18be4eaafc539ca81de1ffd5639181
scw3812 <scw3812@naver.com>
2022-09-22 18:48:45 +0900

    mongoose 세팅

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -3,14 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  private readonly isDev = process.env.MODE === 'dev';
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
   }
 }
--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,16 +1,5 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Put,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { PositiveIntPipe } from '../common/pipes/positiveInt.pipe';
 import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 
 @Controller('cats')
@@ -19,33 +8,7 @@ export class CatsController {
   constructor(private readonly castsService: CatsService) {}
 
   @Get()
-  getAllCat() {
-    return { cats: 'all cat' };
-  }
-
-  @Get(':id')
-  getCat(@Param('id', ParseIntPipe, PositiveIntPipe) id) {
-    console.log(typeof id);
-    return 'one cat';
-  }
-
-  @Post()
-  createCat() {
-    return 'create cat';
-  }
-
-  @Put(':id')
-  updateCat() {
-    return 'update cat';
-  }
-
-  @Patch(':id')
-  updatePartialCat() {
-    return 'update partial cat';
-  }
-
-  @Delete(':id')
-  deleteCat() {
-    return 'delete cat';
+  getCurrentCat() {
+    return 'current cat';
   }
 }
